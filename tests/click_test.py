b33d4fd93190aecb2325c478e1e0ef57fa4529d7
rahpat0211 <80171784+rahpat0211@users.noreply.github.com>
2022-04-19 13:27:48 -0400

    Updated Test file

--- tests/click_test.py
+++ tests/click_test.py
@@ -14,7 +14,7 @@ def test_create_log_folder():
     # set the name of the apps log folder to logs
     logdir = os.path.join(root, '../logs')
     # make a directory if it doesn't exist
-    #assert os.path.exists(logdir) == True
+    assert os.path.exists(logdir) == True
 
 def test_create_database():
     response = runner.invoke(create_database)
@@ -24,3 +24,5 @@ def test_create_database():
     dbdir = os.path.join(root, '../database')
     # make a directory if it doesn't exist
     assert os.path.exists(dbdir) == True
+
+#Test to check if Debug File is created
